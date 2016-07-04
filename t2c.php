a7232286c3fc52a6f02cfedfaf02939b524bd17e
imerina <imerina@nomail.com>
2016-07-04 10:30:42 +0200

    Améliorations

--- t2c.php
+++ t2c.php
@@ -1,30 +1,28 @@
 <?php
+
 /**
  * Génère une classe métier à partir du contenu d'une table MySQL
  * Sous Windows, la ligne commande ressemble à ça
- * php.exe -f t2c.php -- -b base -ttable -c
+ * php.exe -f t2c.php -- -b base -ttable 
  */
-
 /**
  * Vérification des paramètres 
  * -b base de données
  * -t table
- * -c convertit les noms des champs en camel case 
  */
-$options = getopt("b:t:c");  // Récupère les paramètres dans la ligne de commande
+$options = getopt("b:t:");  // Récupère les paramètres dans la ligne de commande
 //var_dump($options) ; //test
-$base=isset($options['b'])? trim($options['b']):'';
-$table=isset($options['t'])? trim($options['t']):'*';
-$isCamelCase=isset($options['c']);
-// @TODO remplacer les valeurs en dur
-$host='localhost';
-$user="root";
-$password="";
+$base = isset($options['b']) ? trim($options['b']) : '';
+$table = isset($options['t']) ? trim($options['t']) : '*';
+// @todo remplacer ces valeurs en dur
+$host = 'localhost';
+$user = "root";
+$password = "";
 
 
 /* echo $base.PHP_EOL; //test
-echo $table.PHP_EOL; //test
-var_dump($isCamelCase); //test
+  echo $table.PHP_EOL; //test
+  var_dump($isCamelCase); //test
  */
 
 /**
@@ -35,51 +33,83 @@ try {
   $dbh = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-  e("Impossible de se connecter à la base '".$base."'".PHP_EOL.$e->getMessage());
+  e("Impossible de se connecter à la base '" . $base . "'" . PHP_EOL . $e->getMessage());
   exit(99);
 }
 /**
- * Lecture du contenu de la table MySQL demandée
+ * Liste des tables 
  */
+if ($table == '*') {
+  try {
+    // Récupère les tables de la base
+    $sql = "show tables";
+    $sth = $dbh->query($sql);
+    $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+  } catch (PDOException $e) {
+    e("Impossible de lire le contenu de la table '" . $table . "'" . PHP_EOL . $e->getMessage());
+    exit(99);
+  }
+  foreach ($rows as $row) {
+    $tables[] = $row['Tables_in_wikipizza'];
+  }
+} else {
+  $tables[] = $table;
+}
+
+
+foreach ($tables as $table) {
+  /**
+   * Lecture du contenu de la table MySQL demandée
+   */
   try {
     // Récupère les colonnes de la table
-    $sql = "show columns from $table";
+    $sql = "show full columns from $table";
     $sth = $dbh->query($sql);
     $colonnes = $sth->fetchAll(PDO::FETCH_ASSOC);
   } catch (PDOException $e) {
-  e("Impossible de lire le contenu de la table '".$table."'".PHP_EOL.$e->getMessage());
-  exit(99); 
+    e("Impossible de lire le contenu de la table '" . $table . "'" . PHP_EOL . $e->getMessage());
+    exit(99);
   }
-/**
- * Génération de la classe métier
- */
+  /**
+   * Génération de la classe métier
+   */
   $date = new DateTime();
   $horodatage = $date->format("d/m/Y à H:i:s");
-  // Entête de classe
+// Entête de classe
   $data = '<?php' . PHP_EOL;
-  $data .= '//' . PHP_EOL;
-  $data .= '// Classe ' . ucfirst($table) . PHP_EOL;
-  $data .= '//   Base      : ' . $base. PHP_EOL;
-  $data .= '//   Table     : ' . $table . PHP_EOL;
-  $data .= '//   Généré le : ' . $horodatage . PHP_EOL;
-  $data .= '//' . PHP_EOL;
+  $data .= '/**' . PHP_EOL;
+  $data .= ' * Classe ' . ucfirst($table) . PHP_EOL;
+  $data .= ' *' . PHP_EOL;
+  $data .= ' *   Base      : ' . $base . PHP_EOL;
+  $data .= ' *   Table     : ' . $table . PHP_EOL;
+  $data .= ' *   Généré le : ' . $horodatage . PHP_EOL;
+  $data .= ' */' . PHP_EOL;
   $data .= 'class ' . ucfirst($table) . ' {' . PHP_EOL;
   $data .= PHP_EOL;
-  // Propriétés
+// Propriétés
   foreach ($colonnes as $colonne) {
+    // Nom de la variable
+    $nom = trim($colonne['Comment'] != '') ? trim($colonne['Comment']) : str_replace('_', ' ', $colonne['Field']);
+    $data.= ' /**' . PHP_EOL;
+    $data.= '  * ' . $nom . PHP_EOL;
+    $data.= '  * @var ' . $colonne['Type'] . PHP_EOL;
+    $data.= '  */' . PHP_EOL;
     $data.= '  private $' . $colonne['Field'] . ';' . PHP_EOL;
   }
-  // constructeur
-  $data .= PHP_EOL;
-  $data .= '  // Constructeur' . PHP_EOL;
-  $data .= '  function __construct($tableau=array()) {' . PHP_EOL;
-  $data .= '    $this->populate($tableau);' . PHP_EOL;
-  $data .= '  }' . PHP_EOL;
-  // Getter/setter
+// Constructeur
+  $data.= PHP_EOL;
+  $data.= ' /**' . PHP_EOL;
+  $data.= '  * Constructeur' . PHP_EOL;
+  $data.= '  */' . PHP_EOL;
+  $data.= '  function __construct($tableau=array()) {' . PHP_EOL;
+  $data.= '    $this->populate($tableau);' . PHP_EOL;
+  $data.= '  }' . PHP_EOL;
+// Getter/setter
+  $data.= PHP_EOL;
+  $data.= ' /**' . PHP_EOL;
+  $data.= '  * Getter/Setter' . PHP_EOL;
+  $data.= '  */' . PHP_EOL;
   foreach ($colonnes as $colonne) {
-    $data .= PHP_EOL;
-    $data .= '  // ' . $colonne['Field'] . PHP_EOL;
-    $data .= '  //   ' . $colonne['Type'] . PHP_EOL;
     $data.= '  function get' . ucfirst($colonne['Field']) . '() {' . PHP_EOL;
     $data.= '    return $this->' . $colonne['Field'] . ';' . PHP_EOL;
     $data.= '  }' . PHP_EOL;
@@ -87,9 +117,11 @@ try {
     $data.= '    $this->' . $colonne['Field'] . '=$' . $colonne['Field'] . ';' . PHP_EOL;
     $data.= '  }' . PHP_EOL;
   }
-  // Hydrateur
-  $data .= PHP_EOL;
-  $data .= '  // Hydrateur' . PHP_EOL;
+// Hydrateur
+  $data.= PHP_EOL;
+  $data.= ' /**' . PHP_EOL;
+  $data.= '  * Hydrateur' . PHP_EOL;
+  $data.= '  */' . PHP_EOL;
   $data.= '  function populate(array $tableau) {' . PHP_EOL;
   $data.= '    foreach ($tableau as $cle => $valeur) {' . PHP_EOL;
   $data.= '      $methode = \'set\'.ucfirst($cle);' . PHP_EOL;
@@ -99,7 +131,7 @@ try {
   $data.= '    }' . PHP_EOL;
   $data.= '  }' . PHP_EOL;
 
-  // Afficheur
+// Afficheur
   $data .= PHP_EOL;
   $data .= '  // Liste des propriétés' . PHP_EOL;
   $data .= '  function display() {' . PHP_EOL;
@@ -109,18 +141,18 @@ try {
   $data .= '    }' . PHP_EOL;
   $data .= '    echo "</ul>".PHP_EOL;' . PHP_EOL;
   $data .= '  }' . PHP_EOL;
-  // Bas de classe
+// Bas de classe
   $data .= PHP_EOL;
   $data .= "} // Classe" . PHP_EOL;
-  // Vérifie que le dossier de destination existe
+// Vérifie que le dossier de destination existe
   $dirname = 'output';
   if (!file_exists($dirname)) {
     mkdir($dirname, 0777);
   }
-  // Génère le fichier
-  $filename = $table.".class.php";
-  file_put_contents($dirname.DIRECTORY_SEPARATOR.$filename, $data);
-
+// Génère le fichier
+  $filename = $table . ".class.php";
+  file_put_contents($dirname . DIRECTORY_SEPARATOR . $filename, $data);
+}
 /**
  * Fonctions communes
  */
@@ -131,5 +163,5 @@ try {
  */
 function e($message) {
   echo iconv("UTF-8", "CP437//IGNORE", $message);
+  //echo iconv("UTF-8", "CP1252//IGNORE", $message);
 }
-
